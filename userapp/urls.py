b2b7c474733ac03ef91fbc2afccf7cd4fbc073cc
KomalKumari17 <komalydv2760@gmail.com>
2025-09-03 22:30:38 +0530

    Add logout functionality: implement LogoutView to blacklist refresh tokens and update URLs

--- userapp/urls.py
+++ userapp/urls.py
@@ -9,6 +9,7 @@ router.register(r'professionals', UserProfileViewSet, basename='vendor')
 urlpatterns = [
     path("", include(router.urls)),
     path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('user-info/', UserInfoView.as_view(), name='user-info'),
     path('register/', RegisterView.as_view(), name='register'),
     path('users/', UserListView.as_view(), name='user-list'),
--- userapp/views.py
+++ userapp/views.py
@@ -7,6 +7,7 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework import viewsets
 from rest_framework.decorators import action
+from rest_framework.authtoken.models import Token
 
 # Create your views here.
 class RegisterView(APIView):
@@ -39,7 +40,8 @@ class LoginView(APIView):
                     "email": user.email,
                     "role": user.role,
                 },
-                "token": str(refresh.access_token)
+                "token": str(refresh.access_token),
+                "refresh": str(refresh)
 
             }, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -106,3 +108,18 @@ class DepartmentListView(APIView):
         departments = Department.objects.all()
         serializer = DepartmentSerializer(departments, many=True)
         return Response(serializer.data)
+    
+
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        refresh_token = request.data.get("refresh_token")
+        if not refresh_token:
+            return Response({"error": "Refresh token is required."}, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response({"message": "Logout successful"}, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
