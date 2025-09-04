3371f7785230757989ba0ac88a737afbde02599b
KomalKumari17 <komalydv2760@gmail.com>
2025-09-04 17:00:46 +0530

    Update CORS and CSRF trusted origins: add https://www.euni.online/ for production

--- euni/settings.py
+++ euni/settings.py
@@ -68,12 +68,14 @@ CORS_ALLOWED_ORIGINS = [
      "http://127.0.0.1:5173",
      "http://localhost:8000",
      "http://127.0.0.1:8000",
+     "https://www.euni.online/"
  ]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://localhost:8000",
+    "https://www.euni.online/"
 ]
 
 SIMPLE_JWT = {
