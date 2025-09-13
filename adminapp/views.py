0040c36d1bd267f490951db3c70180c5b635fc88
KomalKumari17 <komalydv2760@gmail.com>
2025-09-13 22:25:41 +0530

    Refactor destroy method in UserViewSet to use perform_destroy for instance deletion

--- adminapp/views.py
+++ adminapp/views.py
@@ -33,12 +33,13 @@ class UserViewSet(viewsets.ModelViewSet):
             "message": "User updated successfully"
             }, status=status.HTTP_200_OK)
     
-    def perform_destroy(self, instance):
-        instance.delete()
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
         return Response({
             "status": status.HTTP_200_OK,
             "message": "User deleted successfully"
-            }, status=status.HTTP_200_OK)
+        }, status=status.HTTP_200_OK)
 
 class DepartmentViewSet(viewsets.ModelViewSet):
     queryset = Department.objects.all()
