092d753845a1e2214b2ce1ee1814514a6a2928a3
KomalKumari17 <komalydv2760@gmail.com>
2025-09-22 22:02:00 +0530

    Add PaymentAdmin class to manage Payment model in Django admin

--- payment/admin.py
+++ payment/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import Payment
 # Register your models here.
+
+def get_all_fields(model):
+    return [field.name for field in model._meta.get_fields()]
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = get_all_fields(Payment)
+    list_filter = ['status', 'payment_mode', 'created_at']
+    search_fields = ['user__username', 'order_id', 'transaction_id']
+    ordering = ['-created_at']
